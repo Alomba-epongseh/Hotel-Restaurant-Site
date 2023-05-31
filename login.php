2ec1505219e8f44a245a051352e4e4ee3514083a
Alomba-epongseh <alombangwingche@gmail.com>
2023-05-31 14:37:03 +0100

    php code for login page

--- login.php
+++ login.php
@@ -1,89 +1,19 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Vanillier </title>
+<?php
 
-     <!-- Favicon -->
-     <link href="img/favicon.ico" rel="icon">
+    include 'db.php';
 
-    <!-- Google Web Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600&family=Nunito:wght@600;700;800&family=Pacifico&display=swap" rel="stylesheet">
+    $conn = openconn();
 
-    <!-- Icon Font Stylesheet -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+    //echo "Connected successfully";
 
-    <!-- Libraries Stylesheet -->
-    <link href="lib/animate/animate.min.css" rel="stylesheet">
-    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
-    <link href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
-
-    <!-- Customized Bootstrap Stylesheet -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Template Stylesheet -->
-    <link href="css/style.css" rel="stylesheet">
-
-</head>
-<body>
-
-<div class="container-fluid page-header p-5" style="background-image: url(img/about-2-hotel.jpg); background-size: contain; height: 100vh;">
-    <div class="light container-fluid w-50  p-5 rounded " style="background: linear-gradient(rgba(177, 191, 255, 0.49),rgba(177, 191, 255, 0.49));" >
-        <form class="rounded" action="login.php" method="post" >
-        <?php 
-            if (isset($_GET['error'])) { 
-        ?>
-            <p class="error">
-                <?php  
-                    echo $_GET['error']; 
-                ?>
-            </p>
-            <?php 
-                } 
-            ?>
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label" >Username</label>
-                <input type="text" class="form-control" id="exampleInputUsername" aria-describedby="emailHelp"  name="uname">
-            </div>
-            <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1" name="password">
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
-    </div>
-</div>
-
- 
-
-
-    <!-- JavaScript Libraries -->
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="lib/wow/wow.min.js"></script>
-    <script src="lib/easing/easing.min.js"></script>
-    <script src="lib/waypoints/waypoints.min.js"></script>
-    <script src="lib/counterup/counterup.min.js"></script>
-    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
-    <script src="lib/tempusdominus/js/moment.min.js"></script>
-    <script src="lib/tempusdominus/js/moment-timezone.min.js"></script>
-    <script src="lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
-
-    <!-- Template Javascript -->
-    <script src="js/main.js"></script>
-</body>
-</html>
-
+    
+?>
 
 <?php 
 
 session_start(); 
 
-include "db.php";
+//include "db.php";
 
 if (isset($_POST['uname']) && isset($_POST['password'])) {
 
@@ -105,19 +35,19 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
 
     if (empty($uname)) {
 
-        header("Location: index.php?error=User Name is required");
+        header("Location: login.php?error=User Name is required");
 
         exit();
 
     }else if(empty($pass)){
 
-        header("Location: index.php?error=Password is required");
+        header("Location: login.php?error=Password is required");
 
         exit();
 
     }else{
 
-        $sql = "SELECT * FROM users WHERE user_name='$uname' AND password='$pass'";
+        $sql = "SELECT * FROM adminlogin WHERE user_name ='$uname' AND passwordd ='$pass'";
 
         $result = mysqli_query($conn, $sql);
 
@@ -125,41 +55,42 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
 
             $row = mysqli_fetch_assoc($result);
 
-            if ($row['user_name'] === $uname && $row['password'] === $pass) {
-
-                echo "<alert>Logged in</alert>";
+            if ($row['user_name'] === $uname && $row['passwordd'] === $pass) {
 
                 $_SESSION['user_name'] = $row['user_name'];
 
-                $_SESSION['name'] = $row['name'];
+                $_SESSION['admin_name'] = $row['admin_name'];
 
-                $_SESSION['id'] = $row['id'];
+                $_SESSION['Id'] = $row['Id'];
 
-                header("Location: home.php");
-
-                exit();
-
-            }else{
-
-                header("Location: index.php?error=Incorect User name or password");
+                header("Location: admin.php");
 
                 exit();
 
             }
+            else{
 
-        }else{
-
-            header("Location: index.php?error=Incorect User name or password");
-
-            exit();
+                header("Location: indexlogin.php?error=Incorect User name or password");
+        
+                exit();
+        
+            }
 
         }
+        else{
+
+            header("Location: indexlogin.php?error=Incorect User name or password");
+    
+            exit();
+    
+        }
+    
 
     }
 
 }else{
 
-    header("Location: index.php");
+    header("Location: indexlogin.php");
 
     exit();
 
